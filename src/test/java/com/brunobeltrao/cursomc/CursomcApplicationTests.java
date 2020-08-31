fba46468813fdf3587cb9272c12eeffbe160e10a
Bruno Beltrão <brunolbeltrao@gmail.com>
2020-08-31 19:36:55 -0300

    Projeto Criado

---
+++ src/test/java/com/brunobeltrao/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunobeltrao.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
