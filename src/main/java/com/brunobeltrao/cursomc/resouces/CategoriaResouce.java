29feaae86fc4ba03de946b2cb94a7839111d6a97
Bruno Beltrão <brunolbeltrao@gmail.com>
2020-08-31 20:09:43 -0300

    testando a primeira classe de dominio

--- src/main/java/com/brunobeltrao/cursomc/resouces/CategoriaResouce.java
+++ src/main/java/com/brunobeltrao/cursomc/resouces/CategoriaResouce.java
@@ -1,16 +1,28 @@
 package com.brunobeltrao.cursomc.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brunobeltrao.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResouce {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionado";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Teste");
+				
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
